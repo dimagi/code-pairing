e61d0e560a3835bd93062ffbeeb33116d140c97b
Graham Herceg <gherceg@dimagi.com>
2022-12-12 17:34:38 -0500

    rename/reorganize test config files

--- tests/test_pair.py
+++ tests/test_pair.py
@@ -6,17 +6,17 @@ from pair import CodePairs
 class GeneratePairsTests(TestCase):
 
     def test_even_groups_with_even_total(self):
-        cp = CodePairs(config_path='tests/basic.yml')
+        cp = CodePairs(config_path='tests/configs/even_groups.yml')
         pairs = cp._generate_pairs()
         self.assertEqual(len(pairs), 2)
 
     def test_uneven_groups_with_even_total(self):
-        cp = CodePairs(config_path='tests/uneven.yml')
+        cp = CodePairs(config_path='tests/configs/uneven_groups_even_total.yml')
         pairs = cp._generate_pairs()
         self.assertEqual(len(pairs), 3)
 
     def test_uneven_groups_with_odd_total(self):
-        cp = CodePairs(config_path='tests/uneven2.yml')
+        cp = CodePairs(config_path='tests/configs/uneven_groups_odd_total.yml')
         pairs = cp._generate_pairs()
         self.assertEqual(len(pairs), 2)
         self.assertTrue(any([len(p) == 3 for p in pairs]))
