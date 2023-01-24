f876d7e4a0f3161def96d91f86a29ee08e69f6ff
Graham Herceg <gherceg@dimagi.com>
2023-01-24 12:32:08 -0600

    specify content type on message

--- email_utils.py
+++ email_utils.py
@@ -19,7 +19,7 @@ def _build_email(sender, recipients, subject, content):
     message.from_email = sender
     message.to = recipients
     message.subject = subject
-    message.add_content(content)
+    message.add_content(content, 'text/html')
     return message
 
 
