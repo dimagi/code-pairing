072bc3e267292a5fa421fc32563754136fea45ea
Graham Herceg <gherceg@dimagi.com>
2023-01-26 14:04:40 -0600

    revert check for if code should run

--- runner.py
+++ runner.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 from pair import CodePairs
-from utils import should_generate_new_pairs
 
 if __name__ == '__main__':
-    if should_generate_new_pairs():
-        cp = CodePairs()
-        cp.email_pairs()
+    cp = CodePairs()
+    cp.email_pairs()
