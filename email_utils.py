8294f683b86664a1afc7254bc77ace9e8c402f84
Graham Herceg <gherceg@dimagi.com>
2023-01-27 09:11:06 -0600

    revert including explicit emails

--- email_utils.py
+++ email_utils.py
@@ -24,22 +24,22 @@ def _build_email(sender, recipients, subject, content):
 
 
 def _get_recipients_from_usernames(user_infos):
-    return [f"{info['email']}" for info in user_infos]
+    return [f"{info['username']}@dimagi.com" for info in user_infos]
 
 
 def _get_subject(user_infos):
     group_text = 'duo' if len(user_infos) == 2 else 'trio'
-    formatted_usernames = ', '.join([info['name'] for info in user_infos])
+    formatted_usernames = ', '.join([info['username'] for info in user_infos])
     return f'Your code review {group_text}: {formatted_usernames}'
 
 
 def _get_content(user_infos):
     troll_content = ''
     if len(user_infos) == 3:
-        troll_content = constants.TROLL_COPY.format(user_infos[2]['name'],
+        troll_content = constants.TROLL_COPY.format(user_infos[2]['username'],
                                                     user_infos[2]['preference'])
-    return constants.COPY.format(user_infos[0]['name'],
+    return constants.COPY.format(user_infos[0]['username'],
                                  user_infos[0]['preference'],
-                                 user_infos[1]['name'],
+                                 user_infos[1]['username'],
                                  user_infos[1]['preference'],
                                  troll_content)
--- tests/test_email_utils.py
+++ tests/test_email_utils.py
@@ -7,31 +7,24 @@ class SendgridTests(TestCase):
 
     def test_email_for_pair_succeeds(self):
         user_infos = [
-            {'name': 'User', 'email': 'user@dimagi.com',
-             'preference': 'sync'},
-            {'name': 'User2', 'email': 'user2@dimagi.com',
-             'preference': 'async'}]
+            {'username': 'user', 'preference': 'sync'},
+            {'username': 'user2', 'preference': 'async'}]
         message = build_email_from_usernames('sender', user_infos)
         self.assertIsNotNone(message.get())
 
     def test_email_for_trio_succeeds(self):
         user_infos = [
-            {'name': 'User', 'email': 'user@dimagi.com',
-             'preference': 'sync'},
-            {'name': 'User2', 'email': 'user2@dimagi.com',
-             'preference': 'async'},
-            {'name': 'User3', 'email': 'user3@dimagi.com',
-             'preference': 'async'}
+            {'username': 'user', 'preference': 'sync'},
+            {'username': 'user2', 'preference': 'async'},
+            {'username': 'user3', 'preference': 'async'}
         ]
         message = build_email_from_usernames('sender', user_infos)
         self.assertIsNotNone(message.get())
 
     def test_recipients_is_correct(self):
         user_infos = [
-            {'name': 'User', 'email': 'user@dimagi.com',
-             'preference': 'sync'},
-            {'name': 'User2', 'email': 'user2@dimagi.com',
-             'preference': 'async'}]
+            {'username': 'user', 'preference': 'sync'},
+            {'username': 'user2', 'preference': 'async'}]
         message = build_email_from_usernames('sender', user_infos)
         msg_json = message.get()
         recipients = [p['email'] for p in msg_json['personalizations'][0]['to']]
