29b119171e57c37c4ebcd0e972790e62d6fa3d6b
Graham Herceg <gherceg@dimagi.com>
2023-01-30 10:57:55 -0800

    revert config changes back to only username

--- email_utils.py
+++ email_utils.py
@@ -7,10 +7,10 @@ def get_email_client(api_key):
     return sendgrid.SendGridAPIClient(api_key=api_key)
 
 
-def build_email_from_usernames(sender, user_infos):
-    recipients = _get_recipients_from_usernames(user_infos)
-    subject = _get_subject(user_infos)
-    content = _get_content(user_infos)
+def build_email_from_usernames(sender, usernames):
+    recipients = _get_recipients_from_usernames(usernames)
+    subject = _get_subject(usernames)
+    content = _get_content(usernames)
     return _build_email(sender, recipients, subject, content)
 
 
@@ -23,23 +23,20 @@ def _build_email(sender, recipients, subject, content):
     return message
 
 
-def _get_recipients_from_usernames(user_infos):
-    return [f"{info['username']}@dimagi.com" for info in user_infos]
+def _get_recipients_from_usernames(usernames):
+    return [f"{username}@dimagi.com" for username in usernames]
 
 
-def _get_subject(user_infos):
-    group_text = 'duo' if len(user_infos) == 2 else 'trio'
-    formatted_usernames = ', '.join([info['username'] for info in user_infos])
+def _get_subject(usernames):
+    group_text = 'duo' if len(usernames) == 2 else 'trio'
+    formatted_usernames = ', '.join([username for username in usernames])
     return f'Your code review {group_text}: {formatted_usernames}'
 
 
-def _get_content(user_infos):
+def _get_content(usernames):
     troll_content = ''
-    if len(user_infos) == 3:
-        troll_content = constants.TROLL_COPY.format(user_infos[2]['username'],
-                                                    user_infos[2]['preference'])
-    return constants.COPY.format(user_infos[0]['username'],
-                                 user_infos[0]['preference'],
-                                 user_infos[1]['username'],
-                                 user_infos[1]['preference'],
+    if len(usernames) == 3:
+        troll_content = constants.TROLL_COPY.format(usernames[2])
+    return constants.COPY.format(usernames[0],
+                                 usernames[1],
                                  troll_content)
--- tests/test_email_utils.py
+++ tests/test_email_utils.py
@@ -6,26 +6,18 @@ from email_utils import build_email_from_usernames
 class SendgridTests(TestCase):
 
     def test_email_for_pair_succeeds(self):
-        user_infos = [
-            {'username': 'user', 'preference': 'sync'},
-            {'username': 'user2', 'preference': 'async'}]
-        message = build_email_from_usernames('sender', user_infos)
+        usernames = ['user', 'user2']
+        message = build_email_from_usernames('sender', usernames)
         self.assertIsNotNone(message.get())
 
     def test_email_for_trio_succeeds(self):
-        user_infos = [
-            {'username': 'user', 'preference': 'sync'},
-            {'username': 'user2', 'preference': 'async'},
-            {'username': 'user3', 'preference': 'async'}
-        ]
-        message = build_email_from_usernames('sender', user_infos)
+        usernames = ['user', 'user2', 'user3']
+        message = build_email_from_usernames('sender', usernames)
         self.assertIsNotNone(message.get())
 
     def test_recipients_is_correct(self):
-        user_infos = [
-            {'username': 'user', 'preference': 'sync'},
-            {'username': 'user2', 'preference': 'async'}]
-        message = build_email_from_usernames('sender', user_infos)
+        usernames = ['user', 'user2']
+        message = build_email_from_usernames('sender', usernames)
         msg_json = message.get()
         recipients = [p['email'] for p in msg_json['personalizations'][0]['to']]
         self.assertEqual(recipients, ['user@dimagi.com', 'user2@dimagi.com'])
